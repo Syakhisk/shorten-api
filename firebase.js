7b14019aeb811694fdee6b2c12a3af7cae32a371
Syakhisk <mail.syakhisk@gmail.com>
2021-08-29 09:04:16 +0700

    init

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import {initializeApp} from "firebase/app"
+import "dotenv/config"
+
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+};
+
+const app = initializeApp(config)
+
+export const db = app.firestore();
+export default app
+
---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+import routes from "./routes.js"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`)
+})
