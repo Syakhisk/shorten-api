fd68fa3e1ed226edd65d0f661584f6c9fc79ef0d
Syakhisk <mail.syakhisk@gmail.com>
2021-08-31 14:14:51 +0700

    add ratelimit

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const cors = require('cors')
 const routes = require("./routes.js")
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 50 // max per windowms
+});
 
 const app = express()
 app.use(express.static("public"))
+app.use(limiter);
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
